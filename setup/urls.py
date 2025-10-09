ed02ec21bb203dd7e81b2ae2d40b78958d0a9fc9
Vinicius Castro <viniciuscastrodesouza2015@outlook.com>
2025-10-09 02:21:22 -0300

    isolando as url's do app galeria - boa pratica

--- setup/urls.py
+++ setup/urls.py
@@ -1,8 +1,7 @@
 from django.contrib import admin
-from django.urls import path
-from galeria.views import index
+from django.urls import path, include ##include serve para importar as url's do app galeria que foram isoladas o arquivo url's.py dentro da aplicação
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', index)
+    path('', include('galeria.urls')) #utilizando o include para importar as rotas isoladas.
 ]
