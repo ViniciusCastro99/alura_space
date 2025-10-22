bf7ef3f88c5d7031ea460a4b1298c89927a6a5f4
Vinicius Castro <viniciuscastrodesouza2015@outlook.com>
2025-10-21 21:42:32 -0300

    Cria um dicionado com id, nome e legenda das imagems no arquivo view.py e utiliza esse dicionario num loop for dentro do arquivo index.html para renderizar o card das imagens dinâmicamente

--- galeria/views.py
+++ galeria/views.py
@@ -1,8 +1,22 @@
 from django.shortcuts import render
 
 
+
+
 def index(request):
-    return render(request, 'galeria/index.html')
+    ##Cria um dicionario de dicionários com informações da imagens, essas informações serão exíbidas nos cards
+    dados = {
+    1: {
+        "nome": "Nebulosa de Carina",
+        "legenda": "webbtelescope.org / NASA / James Webb"
+        },
+    2: {
+        "nome": "Galáxia NGC 1079",
+        "legenda": "nasa.org / NASA / Hubble"
+       }
+    }
+
+    return render(request, 'galeria/index.html', {"cards": dados}) ##Passa o dicionários de dados das imagens, criado anteriormente.
 
 def imagem(request):
     return render(request, 'galeria/imagem.html')
